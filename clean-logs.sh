076f3f2fa15a97ea808836862cfa429781ab9f36
rb2nem <info@rb2nem.33mail.com>
2016-06-11 12:16:29 +0200

    make log cleaner more specific to nem

--- clean-logs.sh
+++ clean-logs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # delet elog files older than the 9th logfile 
-find . -regextype posix-awk -regex '.*-[[:digit:]]{2}.log' -exec rm \{\} \;
+find . -regextype posix-awk -regex '.*(servant|nis|ncc)-[[:digit:]]{2}.log' -exec rm \{\} \;
