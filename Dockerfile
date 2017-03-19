7f5d5a7bf2473557d5b3e1dc59347f3486de62fa
rb2nem <info@rb2nem.33mail.com>
2017-03-19 11:28:15 +0100

    1.3 builds, needs sed tweaking of gulpfile.js

--- Dockerfile
+++ Dockerfile
@@ -7,8 +7,9 @@ RUN npm cache clean -f && \
     npm install -g gulp-cli
 RUN git clone https://github.com/NemProject/NanoWallet.git && \
     cd NanoWallet && \
-    npm install && \
-    gulp
+    npm install && sed -i -e '/browserSync.init/,+7d' gulpfile.js &&\
+    gulp build-app &&\
+    git checkout gulpfile.js
 RUN rsync -a /NanoWallet/build/ /usr/share/nginx/html
 COPY index.html /usr/share/nginx/html/
 COPY default.conf /etc/nginx/conf.d/default.conf
--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+set -x
+[[ -f Dockerfile ]] || echo "You need to run this script from the clone of the repository, ie the directory holding the Dockerfile"
 docker build -t nanowallet .
-docker run nanowallet tar -c -C /NanoWallet build | tar x
-docker rmi nanowallet
-
+if docker ps -a | grep nanowallet_build >/dev/null ; then
+  docker start nanowallet_build
+else
+  docker run -d -it --name nanowallet_build nanowallet bash
+fi
+docker exec -it nanowallet_build bash -c "cd /NanoWallet && [[ \"$(git rev-parse @{u})\" == \"$(git rev-parse @)\" ]] || ( echo 'updating code' && git pull && npm install && sed -i -e '/browserSync.init/,+7d' gulpfile.js && gulp build-app && checkout gulpfile.js ) "
+docker cp  nanowallet_build:/NanoWallet/build ./
+docker stop nanowallet_build
 echo "open file://$PWD/build/start.html in your browser"
